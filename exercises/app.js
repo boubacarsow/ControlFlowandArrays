bd94f5a1c7e097c33231cf1e594609952a7784a6
boubacarsow <42849861+boubacarsow@users.noreply.github.com>
2022-10-16 19:17:27 -0600

    added if/else statements

--- exercises/app.js
+++ exercises/app.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+//ACCESS GRANTED
+
+const allowedAge = 19;
+
+if(allowedAge >= 18 ) {
+    console.log("access granted. welcome aboard!")
+} else {
+    console.log("You are too young to access this content")
+}
