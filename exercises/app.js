0ac13423ed253e2019da536ffbedfd8564889321
boubacarsow <42849861+boubacarsow@users.noreply.github.com>
2022-10-16 19:28:10 -0600

    letter grade exercise

--- exercises/app.js
+++ exercises/app.js
@@ -1,9 +1,13 @@
 //ACCESS GRANTED
 
-const allowedAge = 19;
+if (condition) {
+    //parent conditional
 
-if(allowedAge >= 18 ) {
-    console.log("access granted. welcome aboard!")
-} else {
-    console.log("You are too young to access this content")
-}
+    //child conditional
+
+    if (conditionToRunIfTrue) {
+    } else {
+    }
+ } else {
+    //if parent condition is true
+ }
